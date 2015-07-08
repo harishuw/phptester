7a8c2e42bd14370d74006e93c719dfd3af07eaa9
Harish U Warrier <harishuw@gmail.com>
2015-07-09 00:44:53 +0530

    phptester v 1.5.0

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 /*
 **************************************
-******PHP Runner 1.0.1****************
+******PHP Runner 1.5.0****************
 ******Created by Harish U Warrier*****
 ******Created on 23-01-2015***********
-******Modified on 14-02-2014**********
+******Modified on 05-07-2015**********
 ******phprunner@jformslider.com*******
 ******php.jformslider.com*************
 **************************************
 */
 $langc='p';
+$version="v 1.5.0";
 if(isset($_POST['code'])){
     if($_POST['lang']=='p'){
             file_put_contents('test.php', '<?php '.$_POST['code'].' ?>');
@@ -28,6 +29,15 @@ if(isset($_GET['function'])){
     $g=trim($_GET['function']);
     call_user_func($g);
     exit;
+}
+if(isset($_POST['json'])){
+    $pjson=$_POST['json'];
+    $pjson=json_decode($pjson,true);
+  
+  echo"<pre>";
+  print_r($pjson);
+  echo"</pre>";
+  exit;
 }
 function save(){
     if(!is_dir('files')){mkdir('files');}
@@ -75,14 +85,15 @@ function download(){
 <!DOCTYPE html>
 <html>
     <head>
-        <title data-start="5000">PHP runner v 1.0.1 Test and run your php,html,javascripts etc</title>
+        <title >PHP Tester <?=$version?> Test and run your php,html,javascripts etc</title>
         <link href="scripts/bootstrap.min.css" rel="stylesheet"/>
-         <style type="text/css">
+        <style type="text/css">
         .just_hide{
             display:none;
         }
         .container{
-            margin-left: 0px !important;
+           /* margin-left: 0px !important;*/
+           margin: 0px 9px;
         }
         .btn-circle{
             border-radius: 50px !important;
@@ -91,9 +102,7 @@ function download(){
             color: #555;
             background-color: #e7e7e7 !important;
         }
-        
         </style>
-        <script src="scripts/jTitleScroller.min.js"></script>
         <script src="scripts/jquery-2.0.3.min.js"></script>
         <script src="scripts/bootstrap.min.js"></script>
     </head>
@@ -101,7 +110,7 @@ function download(){
         <nav class="navbar navbar-default">
           <div class="container-fluid">
             <div class="navbar-header">
-              <a class="navbar-brand" href="#">PHP Runner</a>
+                <a class="navbar-brand" href="#">PHP Runner</a>
             </div>
             <div>
               <ul class="nav navbar-nav">
@@ -109,6 +118,7 @@ function download(){
                 <li><a href="#" id="phpinfo">PHP info</a></li>
                 <li><a href="#" data-toggle="modal" data-target="#notify_me">Notify me</a></li>
                 <li><a href="#" data-toggle="modal" data-target="#text_length">Text Length</a></li>
+		<li><a href="#" onclick='jstoarray()'>JSON to Array</a></li>
                 <li><a href="http://jformslider.com"  target="_blank">jFormslider Plugin</a></li>
               </ul>
             </div>
@@ -116,59 +126,62 @@ function download(){
         </nav>
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <form method="post" class="" role="form" id="runner_form">
-                        <div class="form-group">
-                            <select id="selectcode" class="form-control">
-                                <option value='-1'>Saved Codes</option>
-                                <?php
-                                if(is_dir('files/html')){
-                                    $scd=scandir('files/html');
-                                    for($i=2;$i<count($scd);$i++){?>
-                                        <option data-type="html" value="<?=$scd[$i]?>"><?=$scd[$i]?></option>
-                                <?php }}if(is_dir('files/php')){
-                                    $scd=scandir('files/php');
-                                    for($i=2;$i<count($scd);$i++){?>
-                                        <option data-type="php" value="<?=$scd[$i]?>"><?=$scd[$i]?></option>
-                                <?php }}?>
-                           </select>
+                <div class="col-md-12">
+                    <form method="post" class="form-inline" role="form" id="runner_form">
+                        <ul class="nav nav-tabs" role="tablist" id="mytabs">
+                          <li role="presentation" class="active"><a href="#code" aria-controls="code" role="tab" data-toggle="tab">Code</a></li>
+                          <li role="presentation" onclick="show_result()"><a href="#result_tab" aria-controls="result_tab" role="tab" data-toggle="tab">Result</a></li>
+                          <li>
+                            <div class="form-group">
+                                <select id="selectcode" class="form-control">
+                                    <option value='-1'>Saved Codes</option>
+                                    <?php   if(is_dir('files/html')){
+                                        $scd=scandir('files/html');
+                                        for($i=2;$i<count($scd);$i++){?>
+                                            <option data-type="html" value="<?=$scd[$i]?>"><?=$scd[$i]?></option>
+                                    <?php }}if(is_dir('files/php')){
+                                        $scd=scandir('files/php');
+                                        for($i=2;$i<count($scd);$i++){?>
+                                            <option data-type="php" value="<?=$scd[$i]?>"><?=$scd[$i]?></option>
+                                    <?php }}?>
+                               </select>
+                            </div>
+                            <div class="radio">
+                                <label><input type="radio" value="p" data-type="php" name="lang" <?php if($langc=='p') echo'checked="checked"';?>/>PHP </label>&nbsp;&nbsp;
+                                <label><input type="radio" value="h" data-type="html" name="lang" <?php if($langc=='h') echo'checked="checked"';?>/>HTML</label>
+                                <a class="btn btn-primary btn-xs" onclick="$('#runner_form')[0].reset();$('#selectcode').val('-1');$('#selectcode').change();re_code()"><i class="glyphicon glyphicon-record"></i> Reset</a>
+                                <button class="btn btn-success btn-xs" type="submit" value="1" ><i class="glyphicon glyphicon-play"></i> Run  </button>
+                                <a class="btn btn-success btn-xs" id="savefile" data-placement="bottom"><i class="glyphicon glyphicon-floppy-disk"></i> Save</a>
+                                <a class="btn btn-info btn-xs just_hide file_actions" data-loaing-text="Updating..." id="updatefile"><i class="glyphicon glyphicon-floppy-saved"></i> Update</a>
+                                <a class="btn btn-danger btn-xs just_hide file_actions" data-toggle="modal" data-target="#delete_confirm">
+                                <i class="glyphicon glyphicon-trash" ></i> Delete</a> 
+                                <a href="" id="download_file" class="btn btn-default btn-xs just_hide file_actions"> 
+                                <i class="glyphicon glyphicon-download"></i> Download</a>
+                            </div>
+                              
+                          </li>
+                          <li class=" pull-right">
+                              <i class="glyphicon glyphicon-cog" style="cursor: pointer" data-toggle="modal" data-target="#code_settings"></i>
+                          </li>
+                       </ul>
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane active" id="code">
+                                <textarea class="form-control" name="test" id="type" rows="25" style="width:100%"><?php if(isset($_POST['test'])){echo trim($_POST['test']);}?></textarea>
+                            </div>
+                            <div role="tabpanel" class="tab-pane" id="result_tab">
+                                <div  id="result">
+                                    <?php if(isset($_POST['test'])){
+                                            if($_POST['lang']=='p'){
+                                                file_put_contents('test.php', '<?php '.$_POST['test'].' ?>');
+                                            }else if($_POST['lang']=='h'){
+                                                file_put_contents('test.php', $_POST['test']);
+                                            }
+                                    }
+                                    include_once('test.php');?>
+                                </div>
+                            </div>
                         </div>
-                        <div class="radio">
-                            <label><input type="radio" value="p" data-type="php" name="lang" <?php if($langc=='p') echo'checked="checked"';?>/>PHP </label>&nbsp;&nbsp;
-                            <label><input type="radio" value="h" data-type="html" name="lang" <?php if($langc=='h') echo'checked="checked"';?>/>HTML</label>
-                            <a class="btn btn-primary btn-xs" onclick="$('#runner_form')[0].reset();"><i class="glyphicon glyphicon-record"></i> Reset</a>
-                            <button class="btn btn-success btn-xs" type="submit" value="1" ><i class="glyphicon glyphicon-play"></i> Run  </button>
-                            <a class="btn btn-success btn-xs" id="savefile" data-placement="bottom"><i class="glyphicon glyphicon-floppy-disk"></i> Save</a><br/>
-                            
-                        <a class="btn btn-info btn-xs just_hide file_actions" data-loaing-text="Updating..." id="updatefile"><i class="glyphicon glyphicon-floppy-saved"></i> Update</a>
-                        <a class="btn btn-danger btn-xs just_hide file_actions" data-toggle="modal" data-target="#delete_confirm">
-                        <i class="glyphicon glyphicon-trash  " style="cursor:pointer"/></i> Delete
-                        </a> &nbsp;
-                        
-                        <a href="" id="download_file" class="btn btn-default btn-xs just_hide file_actions"> 
-                        <i class="glyphicon glyphicon-download  " style="cursor:pointer"/></i> Download
-                        </a>
-                        </div>
-                        <textarea class="form-control" name="test" id="type" rows="30" cols=50">
-                            <?php if(isset($_POST['test'])){
-                                    echo trim($_POST['test']);
-                                  }?>
-                        </textarea>
-                        
-                        
                     </form>
-                </div>
-                <div class="col-md-8" id="result">
-                    <?php
-                    if(isset($_POST['test'])){
-                            if($_POST['lang']=='p'){
-                                    file_put_contents('test.php', '<?php '.$_POST['test'].' ?>');
-                            }else if($_POST['lang']=='h'){
-                                    file_put_contents('test.php', $_POST['test']);
-                            }
-                    }
-                    include_once('test.php');
-                    ?>
                 </div>
             </div>
         </div>
@@ -302,11 +315,68 @@ function download(){
             </div>
         </div>
         <!--modals about-->
+        <!--modal settings-->
+         <?php
+         $code_theme="";
+         $code_mode="";
+         $jsace=scandir('scripts/jquery-ace/ace');
+        
+          for($i=0;$i<count($jsace);$i++){
+            
+              if(strstr($jsace[$i],'mode')!=FALSE){
+                  $strmode=  str_replace('mode-', '',$jsace[$i]);
+                   $strmode=  str_replace('.js', '',$strmode);
+                  $strmode= trim($strmode);
+                 $code_mode.="<option value='".$strmode."'>".$strmode."</option>"; 
+              }
+              if(strstr($jsace[$i],'theme')!=FALSE){
+                  $strmode=  str_replace('theme-', '',$jsace[$i]);
+                   $strmode=  str_replace('.js', '',$strmode);
+                   $strmode= trim($strmode);
+                  $code_theme.="<option value='".$strmode."'>".$strmode."</option>";
+              }
+          }
+         ?>
+           <div class="modal fade" id="code_settings">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4>Code Settings</h4>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="form-group">
+                      <label for="code_mode">Code Type:</label>
+                      <select class="form-control" id="code_mode">
+                          <?php echo$code_mode;?>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label for="code_theme">Theme:</label>
+                      <select class="form-control" id="code_theme">
+                          <?php echo$code_theme;?>
+                      </select>
+                    </div>
+                    
+                    <a  class="btn btn-default" onclick="set_theme()" data-dismiss="modal">Save</a>
+
+                 
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default btn-xs" data-dismiss="modal"><i class="glyphicon glyphicon-remove"></i> Close</button>
+                </div>
+              </div>
+            </div>
+        </div>
+         <!--modal settings-->
+        <script src="scripts/jquery-ace/ace/ace.js"></script>
+        <script src="scripts/jquery-ace/jquery-ace.min.js"></script>
         <script src="scripts/tools.js"></script>
-         <footer class="footer">
-      <div class="container">
-        <p class="text-muted"></p>
-      </div>
-    </footer>
+	<footer class="footer">
+            <div class="container">
+              <p class="text-muted"></p>
+            </div>
+        </footer>
     </body>
 </html>
